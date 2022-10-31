c0875144ddccbcb591e064ad86ddcf12f0aab4ff
Jeffrey Turns <51068771+Jeffrey01596@users.noreply.github.com>
2022-11-01 00:11:18 +0200

    fix: display on xxl screen

--- src/components/scaffold/footer/Footer.vue
+++ src/components/scaffold/footer/Footer.vue
@@ -29,32 +29,36 @@ const currentYear = () => new Date().getFullYear();
 </script>
 
 <template>
-    <VFooter class="bg-surface-4 rounded-xl ma-4">
-        <VRow
-            justify="center"
-            align="center"
-            no-gutters>
-            <RouterLink to="/">
-                <RailLogo />
-            </RouterLink>
-            <VCol
-                :cols="12"
-                class="text-center">
-                <VBtn
-                    v-for="it in links"
-                    :key="it.title"
-                    :href="it.url"
-                    variant="text"
-                    class="mx-2">
-                    {{ it.title }}
-                </VBtn>
-            </VCol>
-            <VDivider class="my-2 mx-16" />
-            <VCol
-                class="text-center my-4 font-weight-medium"
-                :cols="12">
-                © {{ currentYear() }} Jeffrey Turns
-            </VCol>
-        </VRow>
-    </VFooter>
+    <div class="d-flex align-center justify-center">
+        <VFooter
+            class="bg-surface-4 rounded-xl ma-4"
+            style="max-width: 1200px">
+            <VRow
+                justify="center"
+                align="center"
+                no-gutters>
+                <RouterLink to="/">
+                    <RailLogo />
+                </RouterLink>
+                <VCol
+                    :cols="12"
+                    class="text-center">
+                    <VBtn
+                        v-for="it in links"
+                        :key="it.title"
+                        :href="it.url"
+                        variant="text"
+                        class="mx-2">
+                        {{ it.title }}
+                    </VBtn>
+                </VCol>
+                <VDivider class="my-2 mx-16" />
+                <VCol
+                    class="text-center my-4 font-weight-medium"
+                    :cols="12">
+                    © {{ currentYear() }} Jeffrey Turns
+                </VCol>
+            </VRow>
+        </VFooter>
+    </div>
 </template>
