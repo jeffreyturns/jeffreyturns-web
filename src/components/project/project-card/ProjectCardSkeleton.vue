60c6ef2ab198523456cfaf54afa10e9ab93ae5c8
Jeffrey Turns <51068771+Jeffrey01596@users.noreply.github.com>
2022-11-05 02:11:49 +0200

    chore: move component styles to const

--- src/components/project/project-card/ProjectCardSkeleton.vue
+++ src/components/project/project-card/ProjectCardSkeleton.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { CSSProperties } from 'vue';
+
 interface ProjectCardSkeletonProps {
     show: boolean;
 }
@@ -6,38 +8,68 @@ interface ProjectCardSkeletonProps {
 const props = withDefaults(defineProps<ProjectCardSkeletonProps>(), {
     show: true
 });
+
+const card: CSSProperties = {
+    height: '360px',
+    width: '400px'
+};
+
+const image: CSSProperties = {
+    height: '200px',
+    width: '400px'
+};
+
+const title: CSSProperties = {
+    height: '32px',
+    width: '64px'
+};
+
+const description1: CSSProperties = {
+    height: '24px',
+    width: '128px'
+};
+
+const description2: CSSProperties = {
+    height: '24px',
+    width: '180px'
+};
+
+const chip: CSSProperties = {
+    height: '32px',
+    width: '56px'
+};
 </script>
 
 <template>
     <div
         v-if="props.show"
-        style="width: 400px; height: 360px"
+        :style="card"
         class="ma-1 bg-surface-1 rounded-lg">
         <div
             class="bg-surface-4 rounded-lg"
-            style="width: 400px; height: 200px"></div>
+            :style="image"></div>
         <VCardItem>
             <div
-                style="height: 32px; width: 64px"
+                :style="title"
                 class="bg-surface-4 rounded-lg" />
         </VCardItem>
 
         <div
-            style="height: 24px; width: 128px"
+            :style="description1"
             class="bg-surface-4 rounded-lg ms-4" />
         <div
-            style="height: 24px; width: 180px"
+            :style="description2"
             class="bg-surface-4 rounded-lg ms-4 mt-1 mb-1" />
 
         <VCardActions class="ms-2">
             <div
-                style="height: 32px; width: 56px"
+                :style="chip"
                 class="bg-surface-4 rounded-lg me-1" />
             <div
-                style="height: 32px; width: 56px"
+                :style="chip"
                 class="bg-surface-4 rounded-lg me-1" />
             <div
-                style="height: 32px; width: 56px"
+                :style="chip"
                 class="bg-surface-4 rounded-lg" />
         </VCardActions>
     </div>
