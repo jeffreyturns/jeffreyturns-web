dfe05d50ee32590c2444d7c31eba977a4f358d85
Jeffrey Turns <51068771+Jeffrey01596@users.noreply.github.com>
2022-11-07 02:57:11 +0200

    feat(styles): improve transitions to m3 spec

--- src/styles/styles.css.ts
+++ src/styles/styles.css.ts
@@ -1,19 +1,21 @@
 import { CSSProperties } from 'vue';
 
-const TRANSITION_TIMING_MS = 200;
+const TRANSITION_TIMING_MS = '200ms';
+
+const CUBIC_BEZIER = 'cubic-bezier(0, 0, 0.2, 1)';
 
 export const bgColorTransition: CSSProperties = {
-    transition: `background-color .${TRANSITION_TIMING_MS}s ease`
+    transition: `background-color ${TRANSITION_TIMING_MS} ${CUBIC_BEZIER}`
 };
 
 export const allTransition: CSSProperties = {
-    transition: `all .${TRANSITION_TIMING_MS}s ease`
+    transition: `all ${TRANSITION_TIMING_MS} ${CUBIC_BEZIER}`
 };
 
 export const borderRadiusTransition: CSSProperties = {
-    transition: `border-radius .${TRANSITION_TIMING_MS}s ease`
+    transition: `border-radius ${TRANSITION_TIMING_MS} ${CUBIC_BEZIER}`
 };
 
 export const transitionOptCalc = () => {
-    return `.${TRANSITION_TIMING_MS}s ease`;
+    return `${TRANSITION_TIMING_MS} ${CUBIC_BEZIER}`;
 };
