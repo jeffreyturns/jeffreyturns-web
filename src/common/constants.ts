86451650b5afe0f75b22508adc1a868a7ce432ed
Jeffrey Turns <51068771+Jeffrey01596@users.noreply.github.com>
2023-02-04 12:00:35 +0200

    feat(theme): add seed colors constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,7 +2,19 @@ export const TITLE_PREFIX_APPENDED = ' • Jeffrey Turns';
 
 export const BASE_API_URL = 'https://jeffreyturns-api.vercel.app/api';
 
-export const THEME_SEED = '#ffd0a3'; //#7a564a 56633e 465e91 6e5d03 616200
+export const THEME_SEED_M3_CATALOG = {
+    BASELINE: '#6750a4',
+    INDIGO: '#3f51b5',
+    BLUE: '#2196f3',
+    TEAL: '#009688',
+    GREEN: '#4caf50',
+    YELLOW: '#ffeb3b',
+    ORANGE: '#ff9800',
+    DEEPORANGE: '#ff5722',
+    PINK: '#e91e63'
+};
+
+export const THEME_SEED = '#ffd0a3'; //#7a564a 56633e 465e91 6e5d03 616200 ffd0a3
 
 export const SEARCH_IDLE_TIME = 1000;
 
